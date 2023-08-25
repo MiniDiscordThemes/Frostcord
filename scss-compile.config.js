207ea4b402fa8a1f2c9f9fdd9d8ed8c0818ff94f
Saltssaumure <themes@saltssaumure.net>
2023-08-25 17:00:58 +0100

    Convert for Replugged

---
+++ scss-compile.config.js
@@ -0,0 +1,9 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+    meta: {
+        name: "Frostcord" /* The official name of your theme. */,
+        scss: "frost" /* The name of your base scss file. */,
+        repo: "Frostcord" /* The name of your theme repository. */,
+        version: "dev" /* The version of your theme. */
+    }
+};
